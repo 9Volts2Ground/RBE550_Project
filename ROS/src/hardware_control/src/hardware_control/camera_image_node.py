5d5b4cc2401e17beac4f5aed95f3fd9ee033f434
9Volts2Ground <s.adam.stringham@gmail.com>
2022-03-30 23:54:51 -0700

    Beginning to add RPi camera capability to camera_image_node.py

--- ROS/src/hardware_control/src/hardware_control/camera_image_node.py
+++ ROS/src/hardware_control/src/hardware_control/camera_image_node.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
-# Basics ROS program to publish real-time streaming
-# video from your built-in webcam
-# Author:
-# - Addison Sears-Collins
+# Basics ROS program to publish real-time streaming video
+# Based on code from:
 # - https://automaticaddison.com
+# - https://pyimagesearch.com/2015/03/30/accessing-the-raspberry-pi-camera-with-opencv-and-python/
 
 # Import the necessary libraries
-import rospy # Python library for ROS
-from sensor_msgs.msg import Image # Image is the message type
 from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
-import cv2 # OpenCV library
+import cv2
+import rospy
+from sensor_msgs.msg import Image
 
 # Custom libraries
+from hardware_constants import hardware_constants
 from hw_topics import hw_topics
 
 #----------------------------------------
 # Set global flags and class instances
+hrd = hardware_constants()
 top = hw_topics()
+
+is_wanda = hrd.is_wanda()
 
 def camera_image_node():
 
@@ -31,14 +34,24 @@ def camera_image_node():
     # Control max rate of image capture, Hz
     rate = rospy.Rate( 10 )
 
-    # Create a VideoCapture object
-    # The argument '0' gets the default webcam.
-    cap = cv2.VideoCapture( 0 )
+    # Create object to capture image
+    if is_wanda:
+        # Use RPi-specific camera libraries
+        from picamera import PiCamera
+        from picamra.array import PiRGBArray
+        cam = PiCamera()
+        resolution = ( 640, 480 )
+        cam.resolution = resolution
+        cam.framerate = 10
+        cam_image = PiRGBArray( cam, resolution )
+    else:
+        # Use generic camera cv2 library
+        cam = cv2.VideoCapture( 0 ) # The argument '0' gets the default webcam
+
+    image_encoding = "bgr8"
 
     # Used to convert between ROS and OpenCV images
     br = CvBridge()
-
-    image_encoding = "bgr8"
 
     # While ROS is still running.
     while not rospy.is_shutdown():
@@ -46,7 +59,12 @@ def camera_image_node():
         # Capture frame-by-frame
         # This method returns True/False as well
         # as the video frame.
-        image_captured, image = cap.read()
+        if is_wanda:
+            frame = cam.capture( cam_image, format='bgr', use_video_port=True )
+            image = frame.array
+            cam_image.truncate(0) # Clear video stream to prep for next frame
+        else:
+            image_captured, image = cam.read()
 
         if image_captured == True:
             # Print debugging information to the terminal
