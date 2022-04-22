4004e71bf926a87bdddd237d14e0d8da5890adc6
9Volts2Ground <s.adam.stringham@gmail.com>
2022-04-22 08:35:47 -0700

    Working on adding body pose to walking gait

--- ROS/src/walk_sense/src/classes/walk_topics.py
+++ ROS/src/walk_sense/src/classes/walk_topics.py
@@ -5,5 +5,6 @@ class walk_topics:
         self.target_states = "target_states" # States from processed image info
         self.target_track = "target_track" # Target track state info
         self.walk_twist = "walk_twist" # Commanded twist vector for walking gait
+        self.body_ground_transform = "body_ground_transform" # Transformation from body to ground frame
         self.processed_image = "processed_image" # Image after processing target search states
         self.range_sensor = "range_sensor" # Measurements from ultrasonic range sensor
