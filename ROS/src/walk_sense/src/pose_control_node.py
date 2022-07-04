4e600a992d8a1a12c7bf56ea3e74771b83226a00
9Volts2Ground <s.adam.stringham@gmail.com>
2022-07-04 09:10:47 -0700

    Body pose now publishes tf2 frame for rviz

--- ROS/src/walk_sense/src/pose_control_node.py
+++ ROS/src/walk_sense/src/pose_control_node.py
@@ -4,11 +4,14 @@ import numpy as np
 import rospy
 import time
 from geometry_msgs.msg import TransformStamped
+import tf2_ros
 
 # Custom libraries
 from classes.gait import gait
+from classes.transformation_frames import transformation_frames
 from classes.walk_topics import walk_topics
 gt = gait()
+tf = transformation_frames()
 w_top = walk_topics()
 
 #==============================================================================
@@ -17,6 +20,11 @@ class pose_control_node():
         rospy.init_node( "pose_control_node", anonymous=True )
 
         self.pose = TransformStamped()
+        self.pose.header.frame_id = tf.ground
+        self.pose.child_frame_id = tf.body
+
+        # tf2 broadcaster
+        self.tf2_broadcaster = tf2_ros.TransformBroadcaster()
 
         self.loop_hz = 1
         self.rate = rospy.Rate( self.loop_hz )
@@ -37,6 +45,11 @@ class pose_control_node():
         self.pose.transform.translation.y = 0.0
         self.pose.transform.translation.z = 0.0
 
+        self.pose.transform.rotation.w = 0.0
+        self.pose.transform.rotation.x = 0.0
+        self.pose.transform.rotation.y = 0.0
+        self.pose.transform.rotation.z = 1.0
+
         desired_speed = 0.05 # m/s
 
         t = time.time()
@@ -49,28 +62,21 @@ class pose_control_node():
 
             # Move the robot vertically
             self.pose.header.stamp = rospy.Time.now()
-            self.pose.transform.translation.z += desired_speed * dt
+            self.pose.transform.translation.z += np.min( [desired_speed * dt, gt.body_height] )
 
-            # print(f"z = {self.pose.transform.translation.z}")
+            self.tf2_broadcaster.sendTransform( self.pose )
 
             self.pub.publish( self.pose )
             self.rate.sleep()
 
     #======================================================
     def stay_standing( self ):
-        # Initialize the robot to start on the ground
-        self.pose.header.stamp = rospy.Time.now()
-        self.pose.transform.translation.x = 0.0
-        self.pose.transform.translation.y = 0.0
-        self.pose.transform.translation.z = gt.body_height
 
-        self.pose.transform.rotation.w = 0.0
-        self.pose.transform.rotation.x = 0.0
-        self.pose.transform.rotation.y = 0.0
-        self.pose.transform.rotation.z = 0.0
-
+        # Maintain old height, don't move it
         while not rospy.is_shutdown():
             self.pose.header.stamp = rospy.Time.now()
+
+            self.tf2_broadcaster.sendTransform( self.pose )
 
             self.pub.publish( self.pose )
             self.rate.sleep()
