Refactored to NWU coordinate frame, fixed body/feet frame quaternions

ROS/src/hardware_control/src/hardware_control/hardware_constants.py
@@ -16,13 +16,13 @@ class hardware_constants:
         self.num_joints = 3 # Number of joints in each leg
         self.num_seeker_joints = 2
 
-        # Vector from body center to hip joints, meters
-        self.s = np.array([ [ -0.055, 0.076162, 0.048 ],        # Front left
-                            [  0.055, 0.076162, 0.048 ],        # Front right
-                            [ -0.083376, 0.0, 0.048 ],         # Middle left
-                            [  0.083376, 0.0, 0.048 ],         # Middle right
-                            [ -0.055, -0.076162, 0.048 ],       # Back left
-                            [  0.055, -0.076162, 0.048 ] ] ).T  # Back right
+        # Vector from body center to hip joints, NWU coordinate frames, meters
+        self.s = np.array([ [ 0.076162, 0.055, 0.048 ],        # Front left
+                            [ 0.076162, -0.055, 0.048 ],        # Front right
+                            [ 0.0, 0.083376, 0.048 ],         # Middle left
+                            [ 0.0, -0.083376, 0.048 ],         # Middle right
+                            [ -0.076162, 0.055, 0.048 ],       # Back left
+                            [ -0.076162, -0.055, 0.048 ] ] ).T  # Back right
 
         # Leg link lengths, m
         self.L1 = 0.03226     # Coxa
@@ -80,12 +80,13 @@ class hardware_constants:
         # Seeker info
         #------------------------------------------------------------
         # Transformation from body frame to el motor axis
-        self.T_body2neck = np.array( [ [0.0, -1.0, 0.0, 0.0],
-                                       [1.0,  0.0, 0.0, self.s[1,0]],
-                                       [0.0,  0.0, 1.0, 0.13858],
-                                       [0.0,  0.0, 0.0, 1.0] ] )
+        self.T_body2neck = np.array( [ [1.0, 0.0, 0.0, self.s[0,0]],
+                                       [0.0, 1.0, 0.0, 0.0],
+                                       [0.0, 0.0, 1.0, 0.13858],
+                                       [0.0, 0.0, 0.0, 1.0] ] )
 
-        self.seeker_extension_length = 0.03226 # Length of extension from el motor axis to camera face
+        # ToDo: measure new Scarlet Witch crown value from CAD model
+        self.seeker_extension_length = 0.045 # Length of extension from el motor axis to camera face
 
         self.seeker_channel = np.array( [1, 0] )
 
@@ -147,5 +148,7 @@ class hardware_constants:
 
         return T_inertial2body
 
-
-
+#==============================================================================
+if __name__ == "__main__":
+    hrd = hardware_constants()
+    print("done")

ROS/src/hardware_control/src/hardware_control/imu_node.py
@@ -43,8 +43,9 @@ class IMU_hardware:
                 accel_data = self.sensor.get_accel_data()
                 gyro_data = self.sensor.get_gyro_data()
 
-                self.accel_measured = np.array( [ accel_data['x'], accel_data['y'], accel_data['z'] ] )
-                self.gyro_measured  = np.array( [ gyro_data['z'],  gyro_data['x'],  gyro_data['y'] ] ) * np.pi/180
+                # ToDo: verify this mapping for NWU coordinate frame
+                self.accel_measured = np.array( [ accel_data['y'], -accel_data['x'], accel_data['z'] ] )
+                self.gyro_measured  = np.array( [ gyro_data['x'], -gyro_data['z'], gyro_data['y'] ] ) * np.pi/180
             except:
                 print("Can't update IMU data")
 

ROS/src/walk_sense/src/classes/gait.py
@@ -22,13 +22,13 @@ class gait:
         # Redesigned for strafe
         self.body_height = 0.05   # How high the body will be relative to the ground, m
 
-        # Where the foot will center around, relative to the body_ground frame
-        self.foot_center = np.array( [ [-0.14, 0.17, 0.0],
-                                       [0.14, 0.17, 0.0],
-                                       [-0.20, 0.0, 0.0],
-                                       [0.20, 0.0, 0.0],
-                                       [-0.14, -0.17, 0.0],
-                                       [0.14, -0.17, 0.0] ] ).T
-        self.foot_height = 0.04 # Z position of foot at peak stride in body_ground frame, meters
+        # Where the foot will center around, relative to the ground frame
+        self.foot_center = np.array( [ [0.17,   0.14, 0.0],
+                                       [0.17,  -0.14, 0.0],
+                                       [0.0,    0.20, 0.0],
+                                       [0.0,   -0.20, 0.0],
+                                       [-0.17,  0.14, 0.0],
+                                       [-0.17, -0.14, 0.0] ] ).T
+        self.foot_height = 0.04 # Z position of foot at peak stride in ground frame, meters
 
         self.max_stride_length = 0.068 # meters

ROS/src/walk_sense/src/functions/foot_position_to_joint_angles.py
@@ -22,14 +22,15 @@ def foot_position_to_joint_angles( foot_position_g, body_ground_transform ):
     """
 
     # Grab transformation matrix between body_ground and body frames
-    # ToDo: fill this in, euler -> R calculation
-    Tbg = quaternion_matrix( [ body_ground_transform.transform.rotation.w,
-                               body_ground_transform.transform.rotation.x,
+    Tbg = quaternion_matrix( [ body_ground_transform.transform.rotation.x,
                                body_ground_transform.transform.rotation.y,
-                               body_ground_transform.transform.rotation.z ] )
+                               body_ground_transform.transform.rotation.z,
+                               body_ground_transform.transform.rotation.w ] )
     Tbg[0:3,3] = -np.array( [ body_ground_transform.transform.translation.x,
                               body_ground_transform.transform.translation.y,
                               body_ground_transform.transform.translation.z ] ) # Mind your negatives
+
+
 
     # Grab joint angles from foot_position_g
     joint_angles = np.zeros( shape=(3,6) ) # Initialize array

ROS/src/walk_sense/src/manual_gait_control_node.py
@@ -153,9 +153,9 @@ class manual_gait_control_node():
         ''' Convert /joy topic into commanded twist vector'''
 
         # Calculate linear vector from left joystick
-        scaled_linear_vec = np.array( [self.xbox.l_stick.right_left,
-                                         self.xbox.l_stick.fwd_back,
-                                         0.0 ] ) * self.max_velocity # No vertical velocity vector. We can't fly yet :/
+        scaled_linear_vec = np.array( [ self.xbox.l_stick.fwd_back,
+                                       -self.xbox.l_stick.right_left,
+                                        0.0 ] ) * self.max_velocity # No vertical velocity vector. We can't fly yet :/
         self.twist.twist.linear.x = scaled_linear_vec[0]
         self.twist.twist.linear.y = scaled_linear_vec[1]
 